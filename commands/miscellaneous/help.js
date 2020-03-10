28bccf3cad1d1cbc3ab5ee6eeb654534203804b6
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-10 02:57:52 +0000

    fixed color dec.

--- commands/miscellaneous/help.js
+++ commands/miscellaneous/help.js
@@ -1,14 +1,13 @@
 /* eslint-disable max-len */
 /* eslint-disable no-plusplus */
 const { MessageEmbed } = require('discord.js');
-const { prefix } = require('../../botconfig.json');
-const { cyan } = require('../../colours.json');
+const { prefix, color } = require('../../botconfig.json');
 
 module.exports = {
   config: {
     name: 'help',
     aliases: ['h', 'halp', 'commands'],
-    usage: '!usage',
+    usage: `${prefix}usage`,
     category: 'miscellaneous',
     description: '',
     accessableby: 'Members',
@@ -28,7 +27,7 @@ module.exports = {
         }
       }
 
-      embed.setColor(cyan)
+      embed.setColor(color)
         .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
         .setTimestamp()
@@ -38,7 +37,7 @@ module.exports = {
     } else {
       const command = bot.commands.get(args[0].toLowerCase()) ? bot.commands.get(args[0].toLowerCase()).config : bot.commands.get(bot.aliases.get(args[0].toLowerCase())).config;
 
-      embed.setColor(cyan)
+      embed.setColor(color)
         .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
         .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.name}\n**Description:** ${command.description || 'No Description'}\n**Usage:** ${command.usage || 'No Usage'}\n**Accessable by:** ${command.accessableby || 'Members'}\n**Aliases:** ${command.aliases ? command.aliases.join(', ') : 'None'}`);
