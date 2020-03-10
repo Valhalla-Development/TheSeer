e35c1dd13371cca85ea281ae58262ab4c77c8dc3
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-03-10 02:57:14 +0000

    included some checks/error notifiers

--- bot.js
+++ bot.js
@@ -1,9 +1,40 @@
 const { Client, Collection } = require('discord.js');
-const { token } = require('./botconfig.json');
+const { token, logging } = require('./botconfig.json');
 
 const bot = new Client();
 
 ['aliases', 'commands'].forEach((x) => bot[x] = new Collection());
 ['console', 'command', 'event'].forEach((x) => require(`./handlers/${x}`)(bot));
+
+if (process.version.slice(1).split('.')[0] < 12) {
+  console.log(
+    new Error(
+      '[The Seer] You must have NodeJS 12 or higher installed on your PC.',
+    ),
+  );
+  process.exit(1);
+}
+
+if (logging !== true && logging !== false) {
+  console.log(
+    new TypeError('[Ragnarok] The \'logging\' value must be true or false.'),
+  );
+  process.exit(1);
+}
+
+if (logging === true) {
+  console.log(
+    '[The Seer] Logging enabled! When someone executes a command, I will log that here.',
+  );
+}
+
+// error notifiers
+bot.on('error', (e) => {
+  console.error(e);
+});
+
+bot.on('warn', (e) => {
+  console.warn(e);
+});
 
 bot.login(token);
