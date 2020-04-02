5f2f6e1ef24ddc7c3f74d5a0caf75e902cf4a835
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-04-02 20:17:16 +0100

    added logging of commands

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,12 +1,44 @@
-const { prefix } = require('../../botconfig.json');
+const moment = require('moment');
+const { prefix, logging } = require('../../botconfig.json');
 
 module.exports = async (bot, message) => {
   if (message.author.bot || message.channel.type === 'dm') return;
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const messageArray = message.content.split(' ');
+  const logArgs = messageArray.slice(1);
+  const oargresult = logArgs.join(' ');
   const cmd = args.shift().toLowerCase();
 
   if (!message.content.startsWith(prefix)) return;
   const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
   if (commandfile) commandfile.run(bot, message, args);
+  if (message.member.guild.me.hasPermission('MANAGE_MESSAGES')) {
+    message.delete();
+  }
+
+  // Logging
+  if (logging === true) {
+    if (!oargresult || oargresult === '') {
+      const LoggingNoArgs = `[\x1b[36m${moment().format(
+        'LLLL',
+      )}\x1b[0m] Command \`${cmd}\` was executed by \x1b[36m${
+        message.author.tag
+      }\x1b[0m (Guild: \x1b[36m${message.guild.name}\x1b[0m)`;
+      if (bot.user.id === '508756879564865539') {
+        bot.channels.cache.get('694680953133596682').send(`${cmd} - was executed by ${message.author.tag} - In guild: ${message.guild.name}`, { code: 'css' });
+      }
+      console.log(LoggingNoArgs);
+    } else {
+      const LoggingArgs = `[\x1b[36m${moment().format(
+        'LLLL',
+      )}\x1b[0m] Command \`${cmd} ${oargresult}\` was executed by \x1b[36m${
+        message.author.tag
+      }\x1b[0m (Guild: \x1b[36m${message.guild.name}\x1b[0m)`;
+      if (bot.user.id === '508756879564865539') {
+        bot.channels.cache.get('694680953133596682').send(`${cmd} ${oargresult} - was executed by ${message.author.tag} - In guild: ${message.guild.name}`, { code: 'css' });
+      }
+      console.log(LoggingArgs);
+    }
+  }
 };
