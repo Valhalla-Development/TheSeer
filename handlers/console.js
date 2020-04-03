f42e8040e3fb0a422f291d617b5e778ea287befb
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2020-04-03 17:19:52 +0100

    unneeded command

--- handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-  const prompt = process.openStdin();
-  prompt.addListener('data', (res) => {
-    const x = res.toString().trim().split(/ +/g);
-    bot.channels.get('555039958121971736').send(x.join(' '));
-  });
-};
