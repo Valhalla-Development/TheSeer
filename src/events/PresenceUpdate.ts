5309935fdc399c4477a68f3e8bca6de6de3e60ff
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-02-17 16:06:19 +0000

    + PresenceUpdate

---
+++ src/events/PresenceUpdate.ts
@@ -0,0 +1,11 @@
+import type { ArgsOf, Client } from 'discordx';
+import { Discord, On } from 'discordx';
+import { ActivityType } from 'discord.js';
+
+@Discord()
+export class PresenceUpdate {
+    @On({ event: 'presenceUpdate' })
+    async onPresenceUpdate([oldPresence, newPresence]: ArgsOf<'presenceUpdate'>, client: Client) {
+        //! TO-DO
+    }
+}
