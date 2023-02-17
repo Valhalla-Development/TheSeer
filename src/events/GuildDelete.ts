a7d315048fb37a55c70949a21a3c81ea6135a23c
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-02-17 16:06:11 +0000

    + GuildDelete

---
+++ src/events/GuildDelete.ts
@@ -0,0 +1,25 @@
+import type { ArgsOf, Client } from 'discordx';
+import { Discord, On } from 'discordx';
+import { ActivityType } from 'discord.js';
+import chalk from 'chalk';
+
+@Discord()
+export class GuildDelete {
+    @On({ event: 'guildDelete' })
+    async onGuildDelete([guild]: ArgsOf<'guildDelete'>, client: Client) {
+        console.log(
+            chalk.white.bold('I have been removed from:'),
+            chalk.red.bold(guild.name),
+            chalk.white.bold('(id:'),
+            chalk.red.bold(guild.id),
+            chalk.white.bold(')'),
+        );
+
+        const count = 1;//! placeholder
+        client.user?.setActivity(`${count.toLocaleString('en')} Bots Across ${client.guilds.cache.size.toLocaleString('en')} Guilds`, {
+            type: ActivityType.Watching,
+        });
+
+        //! Delete all from database
+    }
+}
