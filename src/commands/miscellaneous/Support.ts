65ec2fae6da22dbc0635988f281f8179a9517e28
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-02-18 19:45:07 +0000

    + Support command

---
+++ src/commands/miscellaneous/Support.ts
@@ -0,0 +1,25 @@
+import type { Client } from 'discordx';
+import { Discord, Slash } from 'discordx';
+import type { CommandInteraction } from 'discord.js';
+import {
+    ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder,
+} from 'discord.js';
+import { Category } from '@discordx/utilities';
+
+@Discord()
+@Category('Miscellaneous')
+export class Support {
+    @Slash({ description: 'Displays an invite link to the bots support server.' })
+    async support(interaction: CommandInteraction, client: Client) {
+        if (!interaction.channel) return;
+
+        const embed = new EmbedBuilder()
+            .setColor('#e91e63')
+            .addFields({ name: `**${client.user?.username} - Support**`, value: 'Need support?' });
+
+        const buttonA = new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel('Join Guild').setURL('https://discord.gg/Q3ZhdRJ');
+
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(buttonA);
+        await interaction.reply({ components: [row], embeds: [embed] });
+    }
+}
