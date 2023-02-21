53dab21a31e292989dc04f2eb40782a0460bd6b4
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-02-21 12:19:17 +0000

    ya boi forgot some return statements lmao

--- src/commands/moderation/Add.ts
+++ src/commands/moderation/Add.ts
@@ -1,14 +1,17 @@
 import type { Client } from 'discordx';
 import { Discord, Slash, SlashOption } from 'discordx';
 import type { CommandInteraction, GuildMember } from 'discord.js';
-import { ApplicationCommandOptionType, EmbedBuilder } from 'discord.js';
+import { ApplicationCommandOptionType, EmbedBuilder, PermissionsBitField } from 'discord.js';
 import { Category } from '@discordx/utilities';
 import WatchedBots from '../../mongo/schemas/WatchedBots.js';
 
 @Discord()
 @Category('Moderation')
 export class Add {
-    @Slash({ description: 'Adds a bot to the watchlist' })
+    @Slash({
+        description: 'Adds a bot to the watchlist',
+        defaultMemberPermissions: [PermissionsBitField.Flags.ManageGuild],
+    })
     async add(
         @SlashOption({
             description: 'The bot you wish to monitor',
@@ -16,42 +19,49 @@ export class Add {
             required: true,
             type: ApplicationCommandOptionType.User,
         })
-            user: GuildMember,
+            bot: GuildMember,
             interaction: CommandInteraction,
             client: Client,
     ) {
-        if (user.user.id === client.user?.id) {
+        if (bot.user.id === client.user?.id) {
             const embed = new EmbedBuilder().setColor('#e91e63').addFields({
-                name: `**${client.user.username} - Add**`,
+                name: `**${client.user?.username} - Add**`,
                 value: '**◎ Error:** I can not monitor myself.',
             });
             await interaction.reply({ ephemeral: true, embeds: [embed] });
+            return;
         }
 
-        if (!user.user.bot) {
+        if (!bot.user.bot) {
             const embed = new EmbedBuilder().setColor('#e91e63').addFields({
                 name: `**${client.user?.username} - Add**`,
                 value: '**◎ Error:** The target was not a bot.',
             });
             await interaction.reply({ ephemeral: true, embeds: [embed] });
+            return;
         }
 
         const status = await WatchedBots.findOne({ GuildId: interaction.guild?.id });
 
-        console.log(status);
         const found = status ? status.BotIds : [];
-        if (found.includes(user.user.id)) {
+        if (found.includes(bot.user.id)) {
             const embed = new EmbedBuilder().setColor('#e91e63').addFields({
                 name: `**${client.user?.username} - Add**`,
                 value: '**◎ Error:** The target is already being monitored.',
             });
             await interaction.reply({ ephemeral: true, embeds: [embed] });
         } else {
-            found.push(user.user.id);
+            found.push(bot.user.id);
             await WatchedBots.findOneAndUpdate({ GuildId: interaction.guild?.id }, { BotIds: found }, {
                 new: true,
                 upsert: true,
             });
+
+            const embed = new EmbedBuilder().setColor('#e91e63').addFields({
+                name: `**${client.user?.username} - Channel**`,
+                value: `**◎ Success:** Target ${bot} is now being monitored.`,
+            });
+            await interaction.reply({ ephemeral: true, embeds: [embed] });
         }
     }
 }
