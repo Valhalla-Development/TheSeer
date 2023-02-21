083084e4664be8fac493b7b3eb3473253858c403
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-02-21 12:19:46 +0000

    Set default value on mongoose schema

--- src/mongo/schemas/WatchedBots.ts
+++ src/mongo/schemas/WatchedBots.ts
@@ -2,9 +2,9 @@ import { model, Schema } from 'mongoose';
 
 const WatchedBots = new Schema({
     GuildId: { type: String, unique: true },
-    BotIds: Array,
-    Channel: String,
-    Dm: String,
+    BotIds: { type: Array, default: null },
+    Channel: { type: String, default: null },
+    Dm: { type: String, default: null },
 });
 
 export default model('WatchedBots', WatchedBots, 'WatchedBots');
