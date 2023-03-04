f6cf3da2a1c2e495e289650e3158e103e0cfb271
Ragnar Lothbrok <hudsonjd@live.co.uk>
2023-03-04 14:27:59 +0000

    init - full presence update code

--- src/events/PresenceUpdate.ts
+++ src/events/PresenceUpdate.ts
@@ -1,11 +1,65 @@
 import type { ArgsOf, Client } from 'discordx';
 import { Discord, On } from 'discordx';
-import { ActivityType } from 'discord.js';
+import type { TextBasedChannel } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
+import WatchedBots from '../mongo/schemas/WatchedBots.js';
 
 @Discord()
 export class PresenceUpdate {
     @On({ event: 'presenceUpdate' })
     async onPresenceUpdate([oldPresence, newPresence]: ArgsOf<'presenceUpdate'>, client: Client) {
-        // TODO
+        const watchedBots = await WatchedBots.findOne({ GuildId: newPresence.guild?.id });
+        if (!watchedBots) return;
+
+        const { Channel, Dm, BotIds = [] } = watchedBots;
+
+        const offlineEmbed = new EmbedBuilder()
+            .setTitle('The Seer Report')
+            .setDescription(`<@${newPresence.userId}> is **OFFLINE**`)
+            .setColor('#ff2f2f')
+            .setTimestamp();
+
+        const onlineEmbed = new EmbedBuilder()
+            .setTitle('The Seer Report')
+            .setDescription(`<@${newPresence.userId}> is **ONLINE**`)
+            .setColor('#27d200')
+            .setTimestamp();
+
+        // Status other than offline
+        const statusList = ['online', 'idle', 'dnd'];
+
+        if (!oldPresence?.status || !newPresence?.status || (!Channel && !Dm)) return;
+
+        if (!BotIds.includes(newPresence.userId)) return;
+
+        if (oldPresence.status === newPresence.status || (statusList.includes(oldPresence.status) && statusList.includes(newPresence.status))) return;
+
+        const channel = client.channels.cache.get(Channel) as TextBasedChannel;
+
+        if (newPresence.status === 'offline') {
+            channel?.send({ embeds: [offlineEmbed] });
+            if (Dm) {
+                const user = client.users.cache.get(Dm);
+                if (user) {
+                    try {
+                        await user.send({ embeds: [offlineEmbed] });
+                    } catch {
+                        await WatchedBots.findOneAndUpdate({ GuildId: newPresence.guild?.id }, { Dm: null });
+                    }
+                }
+            }
+        } else {
+            channel?.send({ embeds: [onlineEmbed] });
+            if (Dm) {
+                const user = client.users.cache.get(Dm);
+                if (user) {
+                    try {
+                        await user.send({ embeds: [onlineEmbed] });
+                    } catch {
+                        await WatchedBots.findOneAndUpdate({ GuildId: newPresence.guild?.id }, { Dm: null });
+                    }
+                }
+            }
+        }
     }
 }
